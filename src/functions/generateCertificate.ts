dea4d3d573ab5d41c203e5fc917f081cd359f567
GL-19 <loiolagl19@gmail.com>
2022-06-02 10:14:14 -0300

    Adicionada geração do certificado

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,11 +1,34 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
+import { compile } from "handlebars";
+import dayjs from "dayjs";
+import chromium from "chrome-aws-lambda";
+import { join } from "path";
+import { readFileSync } from "fs";
+
+// const chromium = require("chrome-aws-lambda");
 
 interface ICreateCertificate {
 	id: string;
 	name: string;
 	grade: string;
 }
+
+interface ITemplate {
+	id: string;
+	name: string;
+	grade: string;
+	medal: string;
+	date: string;
+}
+
+const compileTemplate = async (data: ITemplate): Promise<string> => {
+	const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+	const html = readFileSync(filePath, "utf-8");
+
+	return compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
 	const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -31,6 +54,39 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 			},
 		})
 		.promise();
+
+	const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+	const medal = readFileSync(medalPath, "base64");
+
+	const data: ITemplate = {
+		id,
+		grade,
+		name,
+		date: dayjs().format("DD/MM/YYYY"),
+		medal,
+	};
+
+	const content = await compileTemplate(data);
+
+	const browser = await chromium.puppeteer.launch({
+		args: chromium.args,
+		defaultViewport: chromium.defaultViewport,
+		executablePath: await chromium.executablePath,
+	});
+
+	const page = await browser.newPage();
+
+	await page.setContent(content);
+
+	const pdf = await page.pdf({
+		format: "a4",
+		landscape: true,
+		printBackground: true,
+		preferCSSPageSize: true,
+		path: process.env.IS_OFFLINE ? "./certificate.pdf" : null,
+	});
+
+	await browser.close();
 
 	return {
 		statusCode: 201,
