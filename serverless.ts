b682b926388f6a7ded52e9e7ab876370f3197ca2
GL-19 <loiolagl19@gmail.com>
2022-06-02 11:19:10 -0300

    Criado lambda verifyCertificate

--- serverless.ts
+++ serverless.ts
@@ -29,7 +29,10 @@ const serverlessConfiguration: AWS = {
 			},
 		],
 	},
-	// import the function via paths
+	package: {
+		individually: false,
+		include: ["./src/templates/**"],
+	},
 	functions: {
 		generateCertificate: {
 			handler: "src/functions/generateCertificate.handler",
@@ -43,8 +46,19 @@ const serverlessConfiguration: AWS = {
 				},
 			],
 		},
+		verifyCertificate: {
+			handler: "src/functions/verifyCertificate.handler",
+			events: [
+				{
+					http: {
+						path: "verifyCertificate/{id}",
+						method: "get",
+						cors: true,
+					},
+				},
+			],
+		},
 	},
-	package: { individually: true },
 	custom: {
 		esbuild: {
 			bundle: true,
